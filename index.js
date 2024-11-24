86725ae82cd786fbba09316adbe9bd5a683f04d0
Shekhu26001 <shekhar99779977@gmail.com>
2024-11-24 21:24:53 +0530

    change

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ const AuthRouter =require("./Routes/Authrouter")
 const ProductRouter =require("./Routes/Products")
 dotenv.config();
 require("./Models/db")
-app.get("/d",(req,res)=>{
-    res.send("ram")
-})
+
 app.use(bodyParser.json())
 app.use(cors())
 const PORT =process.env.PORT
